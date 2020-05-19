5e43f28daca5a787fcb94c8551af5389386e013a
Shira98 <gul.prannu@gmail.com>
2020-05-19 20:08:39 +0530

    Updated Integration tests.

--- todo/src/test/java/com/practice/todo/integration/ToDoIntegrationTest.java
+++ todo/src/test/java/com/practice/todo/integration/ToDoIntegrationTest.java
@@ -3,38 +3,33 @@ package com.practice.todo.integration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.practice.todo.model.ToDoModel;
 import com.practice.todo.repo.ToDoRepo;
-import org.hamcrest.core.Is;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.any;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import java.util.Optional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
-@AutoConfigureMockMvc
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ToDoIntegrationTest {
 
     @Autowired
-    private MockMvc mock;
-
-    @Autowired
     private ToDoRepo repository;
 
-    //Pre-load the database with a single item before each test case:
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    // Pre-load the database with a single item before each test case:
     @BeforeEach
     public void loadTestData() {
         ToDoModel item = new ToDoModel();
@@ -43,58 +38,187 @@ public class ToDoIntegrationTest {
     }
 
     @Test
-    void testRetrieveItem() throws Exception{
+    void testRetrieveItems() throws Exception{
 
-        //Perform the GET request:
-        mock.perform(get("/ToDo"))
-                //Validate response code
-                .andExpect(status().isOk())
-                .andDo(print( ))
+        // Perform the GET request:
+        ResponseEntity<String> responseEntity = this.restTemplate
+                .getForEntity("http://localhost:" + port + "/ToDo",  String.class);
 
-                //Validate the returned fields:
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].id", Is.is(1)))
-                .andExpect(jsonPath("$[0].item", Is.is("Buy Groceries")))
-                .andExpect(jsonPath("$[0].completed", Is.is(false)));
+        String expected = "[{ \"id\":1,\"item\":\"Buy Groceries\",\"completed\":false }]";
+
+        // Assertions:
+        JSONAssert.assertEquals(expected, responseEntity.getBody(), false);
     }
 
     @Test
-    void testCreateItem() throws Exception {
+    void testCreateItemSuccess() throws Exception {
 
-        //Setup product to create:
+        // Set up item to create:
         ToDoModel item = new ToDoModel();
         item.setItem("Buy TV");
 
-        //Perform the POST request:
-        mock.perform(post("/ToDo")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(item)))
+        // Perform the POST request:
+        ResponseEntity<String> responseEntity = this.restTemplate
+                .postForEntity("http://localhost:" + port + "/ToDo", item, String.class);
 
-                //Validate the response code and content type:
-                .andExpect(status().isCreated())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        String expected = "{ \"id\":2,\"item\":\"Buy TV\",\"completed\":false }";
 
-                //Validate the returned fields:
-                .andExpect(jsonPath("$.id", any(Integer.class)))
-                .andExpect(jsonPath("$.item", is("Buy TV")))
-                .andDo(print())
-                .andExpect(jsonPath("$.completed", is(false)));
+        // Assertions:
+        Assertions.assertEquals(201, responseEntity.getStatusCodeValue());
+        JSONAssert.assertEquals(expected, responseEntity.getBody(), false );
+
+        // Double check this new vehicle has been stored:
+        Optional<ToDoModel> returnedItem = repository.findById(2);
+        Assertions.assertTrue(returnedItem.isPresent());
+        Assertions.assertEquals(2, returnedItem.get().getId());
     }
 
     @Test
-    void testProductDeleteSuccess() throws Exception {
+    void testCreateItemNullFailure() throws Exception{
 
-        //Perform the DELETE request and validate the response code:
-        mock.perform(delete("/ToDo/{id}", 1))
-                .andExpect(status().isOk());
+        // Set up a null item to create:
+        ToDoModel item = new ToDoModel();
+
+        // Perform the POST request:
+        ResponseEntity<String> responseEntity = this.restTemplate
+                .postForEntity("http://localhost:" + port + "/ToDo", item, String.class);
+
+        // Assertions:
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+
     }
 
     @Test
-    void testProductDeleteNotFound() throws Exception {
+    void testCreateItemFieldFailure() throws Exception {
 
-        //Perform the DELETE request and validate the response code:
-        mock.perform(delete("/ToDo/{id}", 99))
-                .andExpect(status().isNotFound());
+        // Set up item to create with a bad JSON string:
+        String Item = "{\"name\":\"John\", \"number\":879, \"completed\":false}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> request = new HttpEntity<String>(Item , headers);
+
+        // Perform the POST request:
+        ResponseEntity<String> responseEntity = this.restTemplate
+                .exchange("http://localhost:" + port + "/ToDo", HttpMethod.POST, request, String.class);
+
+        // Assertions:
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+
+    }
+
+    @Test
+    void testDeleteItemSuccess() throws Exception {
+
+        // Perform the DELETE request:
+        ResponseEntity<Object> responseEntity = this.restTemplate
+                .exchange("http://localhost:" + port + "/ToDo/1", HttpMethod.DELETE, null, Object.class);
+
+        // Assertions:
+        Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        Assertions.assertNull(responseEntity.getBody());
+
+        // Double check the item has been deleted:
+        Optional<ToDoModel> optional = repository.findById(1);
+        Assertions.assertFalse(optional.isPresent());
+    }
+
+    @Test
+    void testDeleteItemNotFound() throws Exception {
+
+        // Perform the DELETE request for a non-existent item:
+        ResponseEntity<Object> responseEntity = this.restTemplate
+                .exchange("http://localhost:" + port + "/ToDo/10", HttpMethod.DELETE, null, Object.class);
+
+        // Assertions:
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+        Assertions.assertNull(responseEntity.getBody());
+    }
+
+    @Test
+    void testUpdateItemSuccess() throws Exception{
+
+        // Set up item to update its "completed" & "item" attributes to "true" & "Buy Veggies":
+        ToDoModel item = new ToDoModel(1,"Buy Veggies", true);
+
+        // Perform the PUT request:
+        this.restTemplate
+                .put("http://localhost:" + port + "/ToDo", item, Object.class);
+
+        String expected = "{ \"id\":1,\"item\":\"Buy Veggies\",\"completed\":true }";
+
+        // Double check the item has been updated:
+        Optional<ToDoModel> returnedItem = repository.findById(1);
+
+        // Assertions:
+        Assertions.assertEquals(1, returnedItem.get().getId());
+        Assertions.assertEquals("Buy Veggies", returnedItem.get().getItem());
+        Assertions.assertTrue(returnedItem.get().isCompleted());
+
+    }
+
+    @Test
+    void testUpdateItemNotFound() throws Exception{
+
+        // Set up item to update with non-existent ID:
+        ToDoModel item = new ToDoModel(12,"Buy Groceries", true);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> request = new HttpEntity<String>(asJsonString(item), headers);
+
+        // Perform the PUT request:
+        ResponseEntity<String> responseEntity = this.restTemplate
+                .exchange("http://localhost:" + port + "/ToDo", HttpMethod.PUT, request, String.class );
+
+        // Assertions:
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+
+    }
+
+    @Test
+    void testUpdateItemFailure() throws Exception{
+
+        // Set up item to update with a bad JSON string:
+        String Item = "{\"id\":1, \"number\":879, \"completed\":false}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> request = new HttpEntity<String>(Item , headers);
+
+        // Perform the POST request:
+        ResponseEntity<String> responseEntity = this.restTemplate
+                .exchange("http://localhost:" + port + "/ToDo", HttpMethod.PUT, request, String.class);
+
+        // Assertions:
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+
+    }
+
+    @Test
+    void testRetrieveItemByIDSuccess() throws Exception{
+
+        // Perform the GET request:
+        ResponseEntity<ToDoModel> responseEntity = this.restTemplate
+                .getForEntity("http://localhost:" + port + "/ToDo/1", ToDoModel.class );
+
+        // Assertions:
+        Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        Assertions.assertEquals(1, responseEntity.getBody().getId() );
+        Assertions.assertEquals("Buy Groceries", responseEntity.getBody().getItem() );
+        Assertions.assertFalse(responseEntity.getBody().isCompleted());
+    }
+
+    @Test
+    void testRetrieveItemByIDNotFound() throws Exception {
+
+        // Perform the GET request:
+        ResponseEntity<String> responseEntity = this.restTemplate
+                .getForEntity("http://localhost:" + port + "/ToDo/10",  String.class );
+
+        // Assertions:
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode() );
+        Assertions.assertNull(responseEntity.getBody());
     }
 
     static String asJsonString(final Object obj) {
@@ -104,4 +228,5 @@ public class ToDoIntegrationTest {
             throw new RuntimeException(e);
         }
     }
+
 }
